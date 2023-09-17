a3fb025332fda54984a0b2becaa8ffa049458756
Davis Kimoto <davis.kimoto@hey.com>
2023-09-17 11:07:08 -0600

    fix: some lsp tweaks

--- nvim/lua/dkimot/plugins/lsp/mason.lua
+++ nvim/lua/dkimot/plugins/lsp/mason.lua
@@ -35,7 +35,8 @@ return {
 				"lua_ls",
 				"emmet_ls",
 				"prismals",
-				"pyright"
+				"pyright",
+				"ruby_ls",
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
