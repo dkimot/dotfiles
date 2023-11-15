ecbc6d6ad72d838367ee766a61f1411e9eb7e763
Davis Kimoto <davis.kimoto@hey.com>
2023-11-15 08:52:44 -0700

    feat: setuprvim-rails

--- nvim/lua/dkimot/plugins/treesitter.lua
+++ nvim/lua/dkimot/plugins/treesitter.lua
@@ -1,57 +1,57 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
-		build = ":TSUpdate",
-		dependencies = {
-			"windwp/nvim-ts-autotag",
-		},
-		config = function()
-			-- import nvim-treesitter plugin
-			local treesitter = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+    },
+    config = function()
+      -- import nvim-treesitter plugin
+      local treesitter = require "nvim-treesitter.configs"
 
-			-- configure treesitter
-			treesitter.setup({ -- enable syntax highlighting
-				highlight = {
-					enable = true,
+      -- configure treesitter
+      treesitter.setup { -- enable syntax highlighting
+        highlight = {
+          enable = true,
 
-					additional_vim_regex_highlighting = false,
-				},
-				-- enable indentation
-				indent = { enable = true },
-				-- enable autotagging (w/ nvim-ts-autotag plugin)
-				autotag = { enable = true },
-				-- ensure these language parsers are installed
-				ensure_installed = {
-					"json",
-					"javascript",
-					"typescript",
-					"tsx",
-					"yaml",
-					"html",
-					"css",
-					"prisma",
-					"markdown",
-					"markdown_inline",
-					"graphql",
-					"bash",
-					"lua",
-					"vim",
-					"dockerfile",
-					"gitignore",
-					"ruby",
-					"rust",
-				},
-				-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-				context_commentstring = {
-					enable = true,
-					enable_autocmd = false,
-				},
-				-- auto install above language parsers
-				auto_install = true,
-			})
+          additional_vim_regex_highlighting = false,
+        },
+        -- enable indentation
+        indent = { enable = true },
+        -- enable autotagging (w/ nvim-ts-autotag plugin)
+        autotag = { enable = true },
+        -- ensure these language parsers are installed
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "graphql",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "ruby",
+          "rust",
+        },
+        -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+        -- auto install above language parsers
+        auto_install = true,
+      }
 
-			vim.cmd([[autocmd FileType ruby setlocal indentkeys-=.]])
-		end,
-	},
+      vim.cmd [[autocmd FileType ruby setlocal indentkeys-=.]]
+    end,
+  },
 }
